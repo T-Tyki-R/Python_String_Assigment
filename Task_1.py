d223ffaec5d9adf151f678d5c5ad191ce9fd040c
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-05-02 21:35:42 -0400

    Task 1 WIP

---
+++ Task_1.py
@@ -0,0 +1,64 @@
+# Product Review Analysis
+
+# Task 1 Psuedocode
+#     Define 2 list vars that stores strings = keywords and reviews
+#     Define a string var that stores a default string
+#     Create a func called keyword_Highlight that takes 2 para
+#         Interate through the review list via loop
+#         Define a condition statement where a keyword string is IN a review
+#             if true -> store a replace() method where you replace the keyword with a upper version inside the string var
+#             print the string var             
+
+keyword_Finder = ["good", "excellent", "bad", "poor", "average"]
+posWord = ["good", "excellent"]
+negWord = ["bad", "poor", "average"]
+python_Reviews = [ "This product is really good. I'm impressed with its quality.", 
+                  "The performance of this product is excellent. Highly recommended!", 
+                  "I had a bad experience with this product. It didn't meet my expectations.", 
+                  "Poor quality product. Wouldn't recommend it to anyone.", 
+                  "The product was average. Nothing extraordinary about it." 
+]   
+
+def keyword_Highlighter(word = "", text = ""):   
+    capWords = ""   
+    for i in range(len(text)):       
+        if word[i].casefold() in text[i].casefold():
+            capWords = text[i].replace(word[i], word[i].upper())
+            print(f"{capWords}") # <- prints out everything but the uppercase of the 4th statement
+#           return f"{capWords}" # <- prints out only 1 statement             
+            
+# Task 2 Psuedocode
+#     Create a func called keyword_Score with 3 para
+#         Define 2 integer vars = posCount and negCount
+#         Interate through the review list via loop
+#               Define a conditional statement where a POSITIVE keyword is IN a review
+#                   if true -> increment posCount var by 1
+#               Define a conditional statement where a NEGATIVE keyword is IN a review
+#                   if true -> increment negCount var by one
+#         return the 2 integer vars
+
+def keyword_Counter(word1, word2, text):
+    posCount = 0
+    negCount = 0
+    for i in range(len(text)):
+        if word1[i] in text[i]:
+            posCount += 1
+        if word2[i] in text[i]:
+            negCount += 1
+    print(f"Postive Reviews Count for Python: {posCount}\nNegative Review Count for Python: {negCount}") 
+# ^ Getting an IndexError 
+
+# Task 3 Psuedocode
+#     Create a func called review_Summary with a para
+#         Interate through the para via loop
+#               Define a conditional statement where a POSITIVE keyword is IN a review
+
+
+def review_Summary(text):
+    for i in range(len(text)):
+        print(f"{text[i].partition(".")[0]}...") 
+              
+
+# print(keyword_Highlighter(keyword_Finder, python_Reviews))
+# print(keyword_Counter(posWord, negWord, python_Reviews))
+print(review_Summary(python_Reviews))
