e613751cd211ae1b49c11d8d88020702e0a0d193
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-05-06 17:52:56 -0400

    Task 1 Completed

--- Task_1.py
+++ Task_1.py
@@ -6,7 +6,7 @@
 #     Create a func called keyword_Highlight that takes 2 para
 #         Interate through the review list via loop
 #         Define a condition statement where a keyword string is IN a review
-#             if true -> store a replace() method where you replace the keyword with a upper version inside the string var
+#             if true -> store a replace() method where you replace the keyword with a uppercase version inside the string var
 #             print the string var             
 
 keyword_finder = ["good", "excellent", "bad", "poor", "average"]
@@ -19,14 +19,15 @@ python_reviews = [ "This product is really good. I'm impressed with its quality.
                   "The product was average. Nothing extraordinary about it." 
 ]   
 
-def keyword_Highlighter(keyword, text):   
-    cap_words = ""   
-    for word in range(len(text)):       
-        if keyword[word] in text[word].lower():
-            x = keyword[word].upper()
-            cap_words += x + " "
-    print(f"{cap_words}") # <- prints out all the keywords in caps, but not the sentences
-           
+def keyword_Highlighter(keywords, reviews):
+    for review in reviews: # look at each sentence individually
+        review = review.lower() # make it all lowercase
+        for keyword in keywords: # loop over keywords so we can see if we get any matches in the string
+            if keyword in review: # if we do have a match, we need to replace it
+                review = review.replace(keyword, keyword.upper())
+        print(review) # prints each sentence for us
+
+
 # Task 2 Psuedocode
 #     Create a func called keyword_Score with 3 para
 #         Define 2 integer vars = posCount and negCount
@@ -38,15 +39,22 @@ def keyword_Highlighter(keyword, text):
 #         return the 2 integer vars
 
 def keyword_Counter(word1, word2, text):
-    posCount = 0
-    negCount = 0
-    for i in range(len(text)):
-        if word1[i] in text[i]:
-            posCount += 1
-        if word2[i] in text[i]:
-            negCount += 1
-    print(f"Postive Reviews Count for Python: {posCount}\nNegative Review Count for Python: {negCount}") 
-# ^ Getting an IndexError 
+    pos_count = 0
+    neg_count = 0
+    review = ""
+    print("\nAfter each review, we'll tally each one as either a positive or negative review\n")
+    for i in text:
+        review = i
+        i = i.lower()
+        for x in word1:
+            if x in i:
+                pos_count += 1
+        for y in word2:
+            if y in i:
+                neg_count += 1
+        print(f"{review}\nPostive Reviews Counter: {pos_count}\nNegative Review Counter: {neg_count}\n")
+    return f"\"Python is an excellent product!\" Users rejoiced as Python was an amzaing product"  if pos_count > neg_count else "\"Avoid using Python at all cost!\" Users deemed Python to be trash..." 
+ 
 
 # Task 3 Psuedocode
 #     Create a func called review_Summary with a para
@@ -55,10 +63,10 @@ def keyword_Counter(word1, word2, text):
 
 
 def review_Summary(text):
-    for i in range(len(text)):
-        print(f"{text[i].partition(".")[0]}...") 
+    for i in text:
+        return f"{i.partition(".")[0]}..." 
               
 
-print(keyword_Highlighter(keyword_finder, python_reviews))
-# print(keyword_Counter(posWord, negWord, python_Reviews))
-# print(review_Summary(python_Reviews))
+# print(keyword_Highlighter(keyword_finder, python_reviews))
+# print(keyword_Counter(pos_word, neg_word, python_reviews))
+print(review_Summary(python_reviews))
